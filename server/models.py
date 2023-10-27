8d8c8a002c179e2ec08dd178ed6ff46cf70fc152
Medrine Mulindi <medrine.mulindi@gmail.com>
2023-10-27 09:48:15 +0300

    defines models

---
+++ server/models.py
@@ -0,0 +1,33 @@
+from flask_sqlalchemy import SQLAlchemy
+
+
+db =SQLAlchemy()
+
+class Restaurant(db.Model):
+    __tablename__ = "restaurants"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    address = db.Column(db.String)
+
+    def __repr__(self):
+        return f"Restaurant: {self.name}, address {self.address}"
+    
+class Pizza(db.Model):
+    __tablename__ = "pizzas"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    ingredients = db.Column(db.String, nullable=False)
+
+class RestaurantPizza(db.Model):
+    __tablename__ = "restaurant_pizzas"
+
+    id = db.Clumn(db.Integer, primary_key=True)
+    price = db.Column(db.Integer, nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey("pizzas.id"))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurants.id"))
+
+
+
+
