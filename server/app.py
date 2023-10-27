dcfab3e00c95acbc5a8b1d971ed4b99145f7dbe7
Medrine Mulindi <medrine.mulindi@gmail.com>
2023-10-27 14:46:17 +0300

    Implements GET /restaurants: flask view

--- server/app.py
+++ server/app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
+from flask_restful import Api, Resource
 
-from models import db
+from models import db, Restaurant
 
 app = Flask(__name__)
 
@@ -12,10 +13,27 @@ app.json.compact = False
 
 migrate = Migrate(app, db)
 
+api =Api(app)
+
 db.init_app(app)
 
+class Restaurants(Resource):
+    def get(self):
+        restaurants = Restaurant.query.all()
 
+        restaurants_list = []
+        for restaurant in restaurants:
+            restaurant_dict = {
+                "id": restaurant.id,
+                "name": restaurant.name,
+                "address": restaurant.address     
+            }
+
+            restaurants_list.append(restaurant_dict)
+
+        return make_response(jsonify(restaurants_list), 200)
+
+api.add_resource(Restaurants, "/restaurants", endpoint="restaurants")
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
-    
