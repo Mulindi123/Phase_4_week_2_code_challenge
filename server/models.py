80732bb8686417b2e112171805b8419d8b9a72b0
Medrine Mulindi <medrine.mulindi@gmail.com>
2023-10-27 10:00:33 +0300

    defines relationships

--- server/models.py
+++ server/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 
 
+
 db =SQLAlchemy()
 
 class Restaurant(db.Model):
@@ -9,6 +10,9 @@ class Restaurant(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     address = db.Column(db.String)
+
+    #relationship
+    pizzas = db.relationship('Pizza', secondary='restaurant_pizzas', back_populates='restaurants')
 
     def __repr__(self):
         return f"Restaurant: {self.name}, address {self.address}"
@@ -20,14 +24,19 @@ class Pizza(db.Model):
     name = db.Column(db.String, nullable=False)
     ingredients = db.Column(db.String, nullable=False)
 
+    #relationship
+    restaurants = db.relationship('Resaurant', secondary='restaurant_pizzas', back_populates='pizzas')
+
+    def __repr__(self):
+        return f"Pizza: {self.name}, ingredients {self.ingredients}"
+
 class RestaurantPizza(db.Model):
     __tablename__ = "restaurant_pizzas"
 
     id = db.Clumn(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
-    pizza_id = db.Column(db.Integer, db.ForeignKey("pizzas.id"))
-    restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurants.id"))
+    pizza_id = db.Column(db.Integer, db.ForeignKey("pizzas.id"))   #relationship
+    restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurants.id")) #relationship
 
-
-
-
+    def __repr__(self):
+        return f"Pizza_price: {self.price}"
