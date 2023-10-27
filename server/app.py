d4f15e652fb9a8f7c63c2ac95443e5e28caf9ec5
Medrine Mulindi <medrine.mulindi@gmail.com>
2023-10-27 17:24:32 +0300

    Implements DELETE /restaurants/:id in app.py

--- server/app.py
+++ server/app.py
@@ -2,7 +2,7 @@ from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
-from models import db, Restaurant
+from models import db, Restaurant, Pizza, RestaurantPizza
 
 app = Flask(__name__)
 
@@ -45,9 +45,31 @@ class RestaurantByID(Resource):
         
             response =  make_response(jsonify(restaurant_dict), 200)
         else:
-            response =  make_response({"error": "Restaurant not fount"}, 404)
+            response =  make_response({"error": "Restaurant not found"}, 404)
 
         return response
+    
+    def delete(self,id):
+        restaurant = Restaurant.query.filter_by(id=id).first()
+
+        if restaurant:
+            associated_pizzas = RestaurantPizza.query.filter_by(restaurant_id = id).all()
+            for pizza in associated_pizzas:
+
+                db.session.delete(pizza)
+                db.session.commit()
+
+            db.session.delete(restaurant)
+            db.session.commit()
+
+            return make_response({}, 204)
+        
+        else:
+            response = make_response({"error": "Restaurant not found"}, 404)
+            return response
+
+
+        
         
 api.add_resource(RestaurantByID, "/restaurants/<int:id>", endpoint="restaurants/<int:id>")
 
