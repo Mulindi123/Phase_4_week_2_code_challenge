c9fa01d31f89656a9d39ec3a24ae081028e084ee
Medrine Mulindi <medrine.mulindi@gmail.com>
2024-07-02 17:26:30 +0300

    Sets up App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route} from 'react-router-dom'
-import "../App.css";
+// import "../App.css";
 import RestaurantPage from "./RestaurantsPage";
 import Landing from "./Landing";
 import Pizzas from './Pizzas';
