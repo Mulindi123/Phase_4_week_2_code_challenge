a52c15c1ffd2d01269fdbbf3be78e7f4e6698d7f
Medrine Mulindi <medrine.mulindi@gmail.com>
2024-07-02 21:53:45 +0300

    styles post form

--- client/src/components/PostRestaurantPizzas.js
+++ client/src/components/PostRestaurantPizzas.js
@@ -48,27 +48,43 @@ const PostRestaurantPizzas = () => {
     }
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <input
-                    onChange={(e) => setPrice(e.target.value)}
-                    type="number"
-                    placeholder="Enter Price"
-                    value={price}
-                />
-                <input
-                    onChange={(e) => setPizzaId(e.target.value)}
-                    type="number"
-                    placeholder="Enter PizzaID"
-                    value={pizzaId}
-                />
-                <input
-                    onChange={(e) => setRestaurantId(e.target.value)}
-                    type="number"
-                    placeholder="Enter Restaurant ID"
-                    value={restaurantId}
-                />
-                <button type="submit">Submit</button>
+        <div className="">
+            <h1 className="text-center text-2xl font-extrabold">Add Pizza</h1>
+            <form onSubmit={handleSubmit} className="bg-slate-400 p-6 rounded-lg shadow-2xl max-w-md mx-auto mt-6 mb-6">
+                <div className="mb-4 grid grid-cols-1 sm:grid-cols-2 gap-0">
+                    <label htmlFor="price" className="block text-gray-700 text-ms font-bold mb-2">Price:</label>
+                    <input
+                        onChange={(e) => setPrice(e.target.value)}
+                        name="price"
+                        type="number"
+                        placeholder="Enter Price"
+                        value={price}
+                        className="shadow-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
+                    />
+                </div>
+                <div className="mb-4 grid grid-cols-1 sm:grid-cols-2 gap-0">
+                    <label htmlFor="pizzaId" className="block text-gray-700 text-ms font-bold mb-2">Pizza Id:</label>
+                    <input
+                        onChange={(e) => setPizzaId(e.target.value)}
+                        name="pizzaId"
+                        type="number"
+                        placeholder="Enter PizzaID"
+                        value={pizzaId}
+                        className="shadow-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
+                    />
+                </div>
+                <div className="mb-4 grid grid-cols-1 sm:grid-cols-2 gap-0">
+                <label htmlFor="price" className="block text-gray-700 text-ms font-bold mb-2">Restaurant Id:</label>
+                    <input
+                        onChange={(e) => setRestaurantId(e.target.value)}
+                        name="restId"
+                        type="number"
+                        placeholder="Enter Restaurant ID"
+                        value={restaurantId}
+                        className="shadow-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
+                    />
+                </div>
+                <button type="submit" className="bg-blue-900 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Submit</button>
             </form>
         </div>
     );
