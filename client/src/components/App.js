501147ca2500b2857b4c507860638fa9a8f459bc
Medrine Mulindi <medrine.mulindi@gmail.com>
2024-07-03 14:45:13 +0300

    test

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,4 @@
 import { Routes, Route} from 'react-router-dom'
-// import "../App.css";
 import RestaurantPage from "./RestaurantsPage";
 import Landing from "./Landing";
 import Pizzas from './Pizzas';
